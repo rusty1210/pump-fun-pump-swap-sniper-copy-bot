da57966c278b125718eadf8904ccd4ad988d6f2e
xtoshi999 <warm624.h5@gmail.com>
2025-07-09 15:18:28 +0900

    chorfe

--- src/common/constants.rs
+++
@@ -1,5 +0,0 @@
-pub const INIT_MSG: &str = "Made by DEVDUDES";
-
-pub const RUN_MSG: &str = "
-        RUNNING BOT.....                                                                                               
-       ";
--- src/engine/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod monitor;
-pub mod swap;
-pub mod copy_trading;
-pub mod transaction_parser;
-pub mod selling_strategy;
